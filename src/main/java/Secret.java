28234093599a03607b38481e0c42b9646dc8eec4
sib888 <90562227+sib888@users.noreply.github.com>
2021-09-12 21:19:22 +0700

    Update Secret.java

--- src/main/java/Secret.java
+++ src/main/java/Secret.java
@@ -1,3 +1,3 @@
 public class Secret {
-  public static String botToken = System.getenv(TOKEN);
+  public static String botToken = System.getenv("ODg2MjYyMzQxMTI0MTE2NTQw.YTzCIA.QkRmVdNVeW3gQm84WWkccIvIo38");
 }
